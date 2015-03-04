57e770c49bcff642292908cfc8c6b2d7e86871ba
Puzzlet Chung <puzzlet@gmail.com>
2015-03-04 06:30:33 +0000

    예제 코드와 인터프리터 테스트용 코드를 분리

--- run-tests.py
+++ run-tests.py
@@ -1,9 +1,20 @@
 import glob
+import os
 import subprocess
 
-for file_path in glob.glob('codes/*.yak'):
+
+patterns = [
+    'yaksok/tests/codes/*.yak',
+    'yaksok/tests/codes/*/*.yak',
+]
+
+for file_path in sum((glob.glob(_) for _ in patterns), []):
     print(file_path, '...', end=' ')
-    target = open(file_path + '.out', 'rb').read()
+    target_file_path = file_path + '.out'
+    if not os.path.exists(target_file_path):
+        print('no .out file!')
+        continue
+    target = open(target_file_path, 'rb').read()
     output = subprocess.check_output(
         ['python3', 'yaksok/yaksok.py', file_path])
     if output == target:
