65da33dee503ebc0b0c3900c49f2e4f36771f08c
ipknHama <ipknhama@gmail.com>
2015-04-12 08:33:41 +0900

    add yaksok.ply to setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='yaksok',
-    packages=['yaksok'],
+    packages=['yaksok', 'yaksok.ply'],
     entry_points = {
         'console_scripts': [
             'yaksok = yaksok.yaksok:main',
