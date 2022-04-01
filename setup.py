1ae6d5532a6ac4c6ffd4b69a6a7f4146f92809f1
Puzzlet Chung <puzzlet@gmail.com>
2022-04-01 08:34:24 +0000

    Python 3.8+ 지원

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='yaksok',
+    python_requires='>=3.6',
     packages=['yaksok', 'yaksok.ply', 'yaksok.modules'],
     package_data = {
         '':[ 'modules/*.yak'],
